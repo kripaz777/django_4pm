69ac4f8e1f829b6a8f85e2abbd166aba32648762
Aayush <aceraayush@gmail.com>
2023-06-15 16:50:00 +0545

    Contact models creating

--- home/models.py
+++ home/models.py
@@ -8,3 +8,20 @@ class Contact(models.Model):
     subject = models.TextField()
     message = models.TextField()
 
+class ContactInformation(models.Model):
+    address1 = models.CharField(max_length=500)
+    address2 = models.CharField(max_length=500)
+    phone_no = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    time = models.CharField(max_length=50)
+
+class Services(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Feedback(models.Model):
+    name = models.CharField(max_length= 300)
+    post = models.CharField(max_length= 300)
+    image = models.ImageField(upload_to='media')
+    comment = models.TextField()
