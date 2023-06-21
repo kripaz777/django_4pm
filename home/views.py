83cff122a724dc4df984340181e3c6da7dd610a8
Aayush <aceraayush@gmail.com>
2023-06-21 16:33:45 +0545

    contact form

--- home/views.py
+++ home/views.py
@@ -11,6 +11,19 @@ def about(request):
     return render(request,'about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        na = request.POST['name']
+        em = request.POST['email']
+        sub = request.POST['subject']
+        mes = request.POST['message']
+        data = Contact.objects.create(
+            name = na,
+            email = em,
+            subject = sub,
+            message = mes
+        )
+        data.save()
+
     return render(request,'contact.html')
 
 def portfolio(request):
